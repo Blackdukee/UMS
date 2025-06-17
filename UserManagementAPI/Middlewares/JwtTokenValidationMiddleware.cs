c0030eab00a404fda647564d51bb8b5c2c255f57
Blackdukee <1213059@dtu.edu.eg>
2025-06-17 19:20:01 +0300

    Update JWT validation middleware to skip validation for additional authentication paths

--- UserManagementAPI/Middlewares/JwtTokenValidationMiddleware.cs
+++ UserManagementAPI/Middlewares/JwtTokenValidationMiddleware.cs
@@ -25,10 +25,12 @@ namespace UserManagementAPI.Middlewares
         public async Task InvokeAsync(HttpContext context)
         {
 
-            if (context.Request.Path.StartsWithSegments("/swagger") || context.Request.Path.StartsWithSegments("/api/v1/ums/login"))
+            if (context.Request.Path.StartsWithSegments("/swagger") ||
+            context.Request.Path.StartsWithSegments("/api/v1/ums/auth/login") ||
+            context.Request.Path.StartsWithSegments("/api/v1/ums/auth/google-login"))
             {
                 // Skip JWT validation for these paths
-
+                _logger.LogInformation("Skipping JWT validation for path: {Path}", context.Request.Path);
                 await _next(context);
                 return;
             }
