1c72c22b8f575e48035184974caab827f1903402
Blackdukee <1213059@dtu.edu.eg>
2025-06-17 17:27:08 +0300

    modified the token expiration time

--- Utilities/obj/Debug/net8.0/Utilities.AssemblyInfo.cs
+++ Utilities/obj/Debug/net8.0/Utilities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c18c19c784e9d1c85628733d8c9a8c11ce20abeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24ce280aa3b919054e93de39efeebc91a2b3df9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Utilities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
